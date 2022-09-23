5207738c74b758e7e1f61d661e893585929cd00f
Chowdhury Tanvir Ahmed <CAhmed@spartaglobal.com>
2022-09-23 22:34:41 +0100

    CRUD Operations on Northwind Database

--- DatabaseFirst/LinqApp/Program.cs
+++ DatabaseFirst/LinqApp/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace LinqApp
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LinqApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var context = new NorthwindContext())
+            {
+                Employee employee = new Employee()
+                {
+                    
+                    FirstName = "Chowdhury",
+                    LastName = "Ahmed"
+                };
+                var employeeQuery = from e in context.Employees
+                               where e.FirstName == employee.FirstName &&
+                               e.LastName == employee.LastName
+                               select e;
+            
+                if (employeeQuery.FirstOrDefault()==null)
+                {
+                    context.Add(employee);
+                    context.SaveChanges();
+                }
+
+                foreach(var e in context.Employees)
+                {
+                    Console.WriteLine($"Employee Name: {e.FirstName} {e.LastName}");
+                }
+                var FindEmployee =context.Employees.Find(1011);
+                if (FindEmployee != null)
+                {
+                    FindEmployee.Country = "UK";
+                    context.SaveChanges();
+                }
+                if (FindEmployee !=null)
+                {
+
+                    context.Employees.Remove(FindEmployee);
+                    context.SaveChanges();
+                }
+             
+
+
+            }
         }
     }
 }
